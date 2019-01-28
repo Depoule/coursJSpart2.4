c1910f796e94bfa296498ec7d53e516dc4eea1fd
Depoule <jpoulet02100@gmail.com>
2019-01-28 14:35:02 +0100

    exo 2.4 fini et corrigé

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// au clic, la fonction resetValues permet d'effacer le contenu de chaque champs de saisi et de remettre les valeurs par defaut
+function resetValues(){
+  document.getElementById('informations').reset();
+}
